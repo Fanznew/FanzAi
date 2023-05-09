ca12c38b10b30f5ad77acee71e590d1cd45255c7
Fanz1234 <duffanblank@gmail.com>
2023-05-09 18:49:27 +0700

    FanzBotAnon

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "5784431540:AAEuT5pL0oh87PRjMuByvgs5UdesXakBQHs") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "Monzky411") # jangan dirubah agar tidak error
+GROUP = os.environ.get("GROUP", "") # 
+CHANNEL = os.environ.get("CHANNEL", "") # 
